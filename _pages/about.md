b6161cfc833a577e8fc639207b9975c107f9db82
Vikram Meyer <vikram@Vikrams-MacBook-Pro.local>
2020-12-02 17:00:28 -0500

    Add class notes structure

--- _pages/about.md
+++ _pages/about.md
@@ -2,7 +2,7 @@
 title: "About"
 permalink: "/about/"
 header:
-    #image: "/assets/"
+    image: "/assets/images/sunset_pond.jpg"
 ---
 
 I'm a computer science and applied mathematics major at Vanderbilt University. I enjoy learning about how the world works and have a particular interest in machine learning and artificial intelligence.
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Math 2500 Overview",
+        "excerpt":"Welcome to the beginning of my class notes for Math 2500 Fall 2020 at Vanderbilt University taught by Professor Bruce Hughes. This course covers Multivariable Calculus and Linear Algebra from a proofs based perspective.        Math 2500 Overview     ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/math2500/",
+        "teaser": null
+      },{
+        "title": "PSCI 2259 Overview",
+        "excerpt":"Welcome to the beginning of my class notes for PSCI 2259 Fall 2020 at Vanderbilt University taught by Professor Alan Wiseman. This course covers game theory from a poltical science perspective.        PSCI 2259 Overview      ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/psci2259/",
+        "teaser": null
+      }]
